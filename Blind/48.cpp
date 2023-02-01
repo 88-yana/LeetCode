8d9989ae79fde790b988be5dd962221fdb137b59
88-yana <85784752+88-yana@users.noreply.github.com>
2023-02-01 19:04:15 +0900

    refactor

--- Blind/48.cpp
+++ Blind/48.cpp
@@ -9,21 +9,25 @@ public:
 	void DFS(vector<vector<char>>& board, vector<string>& words, int i, int j, int k, int index) {
 		const bool is_in_y_range = 0 <= i && i < board.size();
 		const bool is_in_x_range = 0 <= j && j < board[0].size();
-		if (seen[k])
-			return ;
 		if (!is_in_y_range || !is_in_x_range)
 			return ;
+		if (seen[k])
+			return ;
+		char temp;
+		temp = board[i][j];
 		if (board[i][j] == words[k][index]) {
 			if (index == words[k].size() - 1) {
 				seen[k] = true;
 				ans.push_back(words[k]);
 				return ;
 			}
+			board[i][j] = 'A';
 			DFS(board, words, i - 1, j, k, index + 1);
 			DFS(board, words, i + 1, j, k, index + 1);
 			DFS(board, words, i, j - 1, k, index + 1);
 			DFS(board, words, i, j + 1, k, index + 1);
 		}
+		board[i][j] = temp;
 	}
 	vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
 		seen.assign(words.size(), false);
@@ -48,6 +52,10 @@ int main(void)
 		{'i','f','l','v'}
 	};
 	vector<string> words = {"oath","pea","eat","rain"};
+	// vector<vector<char>> board = {
+	// 	{'a','a'},
+	// };
+	// vector<string> words = {"aaa"};
 	vector<string> ans;
 	Solution sol;
 

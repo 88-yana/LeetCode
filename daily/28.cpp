4bc8a635f67a75ee01b9a912c6c5d184027d9ff3
88-yana <85784752+88-yana@users.noreply.github.com>
2023-03-03 23:05:51 +0900

    need search kmp

--- daily/28.cpp
+++ daily/28.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-// class Solution {
-// public:
-// 	int strStr(string haystack, string needle) {
-// 		return haystack.find(needle);
-// 	}
-// };
+class Solution {
+public:
+	int strStr(string haystack, string needle) {
+		return haystack.find(needle);
+	}
+};
 
-// class Solution {
-// public:
-// 	int strStr(string haystack, string needle) {
-// 		for (int i = 0; i < haystack.length(); i++) {
-// 			if (haystack.substr(i, needle.size()) == needle)
-// 				return i;
-// 		}
-// 		return -1;
-// 	}
-// };
+class Solution {
+public:
+	int strStr(string haystack, string needle) {
+		for (int i = 0; i < haystack.length(); i++) {
+			if (haystack.substr(i, needle.size()) == needle)
+				return i;
+		}
+		return -1;
+	}
+};
 
 class Solution {
 public:

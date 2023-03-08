7bb6e36ec22af306866c2b68a85f587983c843a1
88-yana <85784752+88-yana@users.noreply.github.com>
2023-03-09 01:20:47 +0900

    binary search

--- daily/1539.cpp
+++ daily/1539.cpp
@@ -4,35 +4,6 @@ using namespace std;
 class Solution {
 public:
 	int findKthPositive(vector<int>& arr, int k) {
-		int mn = 0;
-		for (int i = 0; i < arr.size(); i++) {
-			if (arr[i] - mn > 1) {
-				k -= arr[i] - mn + 1;
-				if (k <= 0)
-					return arr[i] - k;
-			}
-			mn = arr[i];
-		}
-		return mn + k;
-	}
-};
-
-class Solution {
-public:
-	int findKthPositive(vector<int>& arr, int k) {
-		for (int i = 0; i < arr.size(); i++) {
-			int diff = arr[i] - (i + 1);
-			if (diff >= k) {
-				return arr[i] - 1 - (diff - k);
-			}
-		}
-		return arr[arr.size() - 1] - (arr[arr.size() - 1] - (arr.size() - 1 + 1) - k);
-	}
-};
-
-class Solution {
-public:
-	int findKthPositive(vector<int>& arr, int k) {
 		for (int i = 0; i < arr.size(); i++) {
 			if (arr[i] - i > k) {
 				return i + k;
@@ -41,3 +12,46 @@ public:
 		return arr.size() + k;
 	}
 };
+
+arr    2  3  4  7  10
+i      0  1  2  3  4
+//diff 2  2  2  4  6
+
+
+arr    2  3  4  7  11
+i      0  1  2  3  4
+//diff 2  2  2  4  7
+
+
+arr    1  2  4  5  6
+i      0  1  2  3  4
+//diff 1  1  2  2  2
+
+// class Solution {
+// public:
+// 	int findKthPositive(vector<int>& arr, int k) {
+// 		int mn = 0;
+// 		for (int i = 0; i < arr.size(); i++) {
+// 			if (arr[i] - mn > 1) {
+// 				k -= arr[i] - mn + 1;
+// 				if (k <= 0)
+// 					return arr[i] - k;
+// 			}
+// 			mn = arr[i];
+// 		}
+// 		return mn + k;
+// 	}
+// };
+
+// class Solution {
+// public:
+// 	int findKthPositive(vector<int>& arr, int k) {
+// 		for (int i = 0; i < arr.size(); i++) {
+// 			int diff = arr[i] - (i + 1);
+// 			if (diff >= k) {
+// 				return arr[i] - 1 - (diff - k);
+// 			}
+// 		}
+// 		return arr[arr.size() - 1] - (arr[arr.size() - 1] - (arr.size() - 1 + 1) - k);
+// 	}
+// };
---
+++ daily/875.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	bool judge(vector<int>& piles, int& k, int& h) {
+		int times = 0;
+		for (int i = 0; i < piles.size(); i++) {
+			times += (piles[i] + k - 1) / k;
+			if (times > h) {
+				return false;
+			}
+		}
+		return true;
+	}
+	int minEatingSpeed(vector<int>& piles, int h) {
+		int ok = *max_element(piles.begin(), piles.end());
+		int ng = 0;
+		while (ok - ng > 1) {
+			int mid = ng + (ok - ng) / 2;
+			if (judge(piles, mid, h)) {
+				ok = mid;
+			} else {
+				ng = mid;
+			}
+		}
+		return ok;
+	}
+};
